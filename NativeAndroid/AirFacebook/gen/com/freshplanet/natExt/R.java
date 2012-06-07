4a6dc85f51425c58bd9b2b21f79e0efabda09030
Thibaut Crenn <crenn@freshplanet.com>
2012-06-07 19:21:21 -0400

    adds post to fb wall. Fix iOS token expiration timestamp.

--- NativeAndroid/AirFacebook/gen/com/freshplanet/natExt/R.java
+++ NativeAndroid/AirFacebook/gen/com/freshplanet/natExt/R.java
@@ -14,12 +14,25 @@ public final class R {
         public static final int close=0x7f020000;
         public static final int facebook_icon=0x7f020001;
         public static final int ic_launcher2=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int icon_status=0x7f020004;
+    }
+    public static final class id {
+        public static final int image=0x7f060001;
+        public static final int layout=0x7f060000;
+        public static final int text=0x7f060003;
+        public static final int title=0x7f060002;
     }
     public static final class layout {
         public static final int fb_main=0x7f030000;
+        public static final int notification=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int NotificationText=0x7f050000;
+        public static final int NotificationTitle=0x7f050001;
     }
 }
