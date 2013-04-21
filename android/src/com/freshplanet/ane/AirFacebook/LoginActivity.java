2e693496007e104e83f56e5738da4808bf655e86
Kirill Kuvaldin <kuvkir@me.com>
2013-04-21 11:52:49 +0400

    fix "an attempt was made to open an already opened session"

--- android/src/com/freshplanet/ane/AirFacebook/LoginActivity.java
+++ android/src/com/freshplanet/ane/AirFacebook/LoginActivity.java
@@ -58,6 +58,8 @@ public class LoginActivity extends Activity
 
 		reauthorize = extras.getBoolean("reauthorize", false);
 		
+		AirFacebookExtension.log("INFO - LoginActivity.onCreate, session.isClosed " + AirFacebookExtensionContext.session.isClosed() + ", state " + AirFacebookExtensionContext.session.getState());
+		
 		// Authorize Facebook session if necessary
 		if (reauthorize) {
 			if ("read".equals(type)) {
