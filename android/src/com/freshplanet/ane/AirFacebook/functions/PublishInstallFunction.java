6669c3e695b3f8f19449ecf7e2eb4c76fc177519
Thibaut Crenn <crenn@freshplanet.com>
2013-05-28 15:40:27 -0400

    implements publish install for Android

--- android/src/com/freshplanet/ane/AirFacebook/functions/PublishInstallFunction.java
+++ android/src/com/freshplanet/ane/AirFacebook/functions/PublishInstallFunction.java
@@ -2,12 +2,41 @@ package com.freshplanet.ane.AirFacebook.functions;
 
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREInvalidObjectException;
 import com.adobe.fre.FREObject;
+import com.adobe.fre.FRETypeMismatchException;
+import com.adobe.fre.FREWrongThreadException;
+import com.facebook.Settings;
+import com.freshplanet.ane.AirFacebook.AirFacebookExtension;
 
 public class PublishInstallFunction implements FREFunction {
 
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) {
+		
+		
+		String applicationId = null;
+		
+		try {
+			applicationId = arg1[0].getAsString();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (FRETypeMismatchException e) {
+			e.printStackTrace();
+		} catch (FREInvalidObjectException e) {
+			e.printStackTrace();
+		} catch (FREWrongThreadException e) {
+			e.printStackTrace();
+		}
+		
+		if (applicationId != null)
+		{
+			Settings.publishInstallAsync(arg0.getActivity(), applicationId);
+		} else
+		{
+			AirFacebookExtension.log("cannot start publish install, applicationId is null");
+		}
+		
 		return null;
 	}
 
