329962afcfad15b1a31aa76454fe9558d83045f5
Nigam Shah <nigam@freshplanet.com>
2014-07-30 10:38:43 -0400

    update Android part of ANE to refect the change from publishInstall => activateApp

--- android/src/com/freshplanet/ane/AirFacebook/functions/ActivateAppFunction.java
+++ android/src/com/freshplanet/ane/AirFacebook/functions/ActivateAppFunction.java
@@ -1,18 +1,18 @@
+
 package com.freshplanet.ane.AirFacebook.functions;
 
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREObject;
-import com.facebook.Settings;
+import com.facebook.AppEventsLogger;
 
-public class PublishInstallFunction extends BaseFunction
+public class ActivateAppFunction extends BaseFunction
 {
 	@Override
 	public FREObject call(FREContext context, FREObject[] args)
 	{
 		super.call(context, args);
-		
-		String appID = getStringFromFREObject(args[0]);
-		Settings.publishInstallAsync(context.getActivity(), appID);
+
+		AppEventsLogger.activateApp(context.getActivity());
 		
 		return null;
 	}
