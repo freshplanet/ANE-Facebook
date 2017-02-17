a7103a35cb2f730d796e28bc2e6e687b149139cd
Adam Schlesinger <adam@freshplanet.com>
2017-02-17 14:41:10 -0500

    fixed/renamed jar extract script

--- build/extractJars.sh
+++ build/extractJars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-libs_collection_path="../android/build/intermediates/exploded-aar"
+libs_collection_path="../android/build/intermediates/exploded-aar/"
 internal_jar_path="/jars/classes.jar"
 
-find ${libs_collection_path}/* -name "classes.jar" | while read file_path
+find ${libs_collection_path}* -name "classes.jar" | while read file_path
 do
     extricated_of_jar_path=${file_path%$internal_jar_path}
     extricated_of_collection_path=${extricated_of_jar_path#$libs_collection_path}
