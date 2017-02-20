f9aeb62d9e24e4821b7278e1add9f11ea985e478
Adam Schlesinger <adam@freshplanet.com>
2017-02-20 16:28:20 -0500

    updated jar rip script to pull everything

--- build/extractJars.sh
+++ build/extractJars.sh
@@ -3,13 +3,20 @@
 libs_collection_path="../android/build/intermediates/exploded-aar/"
 internal_jar_path="/jars/classes.jar"
 
-find ${libs_collection_path}* -name "classes.jar" | while read file_path
+find ${libs_collection_path}* -name "*.jar" | while read file_path
 do
-    extricated_of_jar_path=${file_path%$internal_jar_path}
-    extricated_of_collection_path=${extricated_of_jar_path#$libs_collection_path}
-    extracted_lib_path=${extricated_of_collection_path#*/}
-    lib_name=${extracted_lib_path////-}
+    echo "copying $file_path"
 
-    echo copying $lib_name
-    cp $file_path "temp/android/$lib_name.jar"
+    extricated_of_collection_path=${file_path#$libs_collection_path}
+
+    if [[ $file_path == *"classes.jar"* ]]; then
+
+        extricated_of_jar_path=${extricated_of_collection_path%$internal_jar_path}
+        extracted_lib_path=${extricated_of_jar_path#*/}
+        lib_name=${extracted_lib_path////-}
+
+        cp $file_path "temp/android/$lib_name.jar"
+    else
+        cp $file_path "temp/android/"
+    fi
 done
