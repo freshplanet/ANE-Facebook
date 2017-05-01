64c89c90ea3ae626172bde5bce04d7d690d12768
Adam Schlesinger <adam@freshplanet.com>
2017-05-01 14:41:08 -0400

    ios - formatting + resolve login deprecations

--- ios/AirFacebook/FBShareDelegate.h
+++ ios/AirFacebook/FBShareDelegate.h
@@ -1,18 +1,24 @@
-//
-//  FBShareDelegate.h
-//  AirFacebook
-//
-//  Created by Ján Horváth on 16/06/15.
-//
-//
+/**
+ * Copyright 2017 FreshPlanet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <Foundation/Foundation.h>
 #import <FBSDKShareKit/FBSDKShareKit.h>
-#import "AirFacebook.h"
 
 @interface FBShareDelegate : NSObject<FBSDKSharingDelegate>
 
-- (id)initWithCallback:(NSString *)aCallback;
-- (BOOL)shareContent:(FBSDKShareLinkContent *)content usingShareApi:(BOOL)useShareApi;
+- (id)initWithCallback:(NSString*)aCallback;
+- (BOOL)shareContent:(FBSDKShareLinkContent*)content usingShareApi:(BOOL)useShareApi;
 
 @end
